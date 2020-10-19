aaa336b479361141fbea773cc73d2bf6ac2521d7
rajadr <73127377+rajadr@users.noreply.github.com>
2020-10-20 00:35:44 +0200

    Update ConversionController.java

--- src/main/java/com/ps/conversion/ConversionController.java
+++ src/main/java/com/ps/conversion/ConversionController.java
@@ -24,4 +24,49 @@ public class ConversionController {
         return temp_in_c;
     }
 
+    // Conversion - Celcius to Kelvin
+    @GetMapping("/conversions/ctok")
+    public double ctok(@RequestParam(value = "temp", defaultValue = "0.0") String temp) {
+        double temp_in_c  = Double.valueOf(temp);
+
+        // Kelvin = Celcium + 273.15
+
+        double temp_in_k = temp_in_c + 273.15;
+        String tempVal = String.format("%1.2f", temp_in_k);
+        temp_in_k = Double.valueOf(tempVal);
+
+        System.out.println("Celcius Temp: "+temp+" and Equivalent Kelvin: "+temp_in_k);
+
+        return temp_in_k;
+    }
+
+    // Conversion - miles to kilometers
+    @GetMapping("/conversions/mtok")
+    public Double mtok(@RequestParam(value = "dist", defaultValue = "0.0") String dist) {
+        double dist_in_mile  = Double.valueOf(dist);
+
+        // 1 mile = 1.609344 kms
+        double dist_in_km = dist_in_mile * 1.609344;
+        String tempVal = String.format("%1.2f", dist_in_km);
+        dist_in_km = Double.valueOf(tempVal);
+
+        System.out.println(dist+" Miles are equivalent to : "+dist_in_km+" Kilometers");
+
+        return dist_in_km;
+    }
+    // Conversion - kilometers to miles
+    @GetMapping("/conversions/ktom")
+    public double ktom(@RequestParam(value = "dist", defaultValue = "0.0") String dist) {
+        double dist_in_km  = Double.valueOf(dist);
+
+        // 1 km = 0.621371 mile
+        double dist_in_mile = dist_in_km * 0.621371;
+        String tempVal = String.format("%1.2f", dist_in_mile);
+        dist_in_mile = Double.valueOf(tempVal);
+
+        System.out.println(dist+" Kilometers are Equivalent to"+dist_in_mile+" Miles");
+
+        return dist_in_mile;
+    }
+
 }
