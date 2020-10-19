824298f93d2c792d857fc4535815f1b5808ce7ce
rajadr <73127377+rajadr@users.noreply.github.com>
2020-10-19 23:48:33 +0200

    Initial Version

---
+++ src/test/java/com/ps/conversion/ConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
